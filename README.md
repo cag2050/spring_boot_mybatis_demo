6b21a05c94e3f15ecf0834cdeaadb293d417c778
cag2050 <cag2050@163.com>
2017-05-23 16:21:51 +0800

    m

---
+++ README.md
@@ -0,0 +1,7 @@
+1. 在 pom.xml 添加相应的内容；  
+1. 在 application.properties 中配置数据库相关信息；  
+1. 在 mysql 建库和建表；  
+1. 写 User.java 和 UserMapper.java;  
+1. 启动应用后，访问 http://localhost:8080/user，只能访问一次；再次访问会向数据库中再插入一条记录，会报错：TooManyResultsException: Expected one result (or null) to be returned by selectOne(), but found: [重复记录个数]；可将数据表中的记录删除，再访问。  
+
+* 使用 junit 测试（ApplicationTests.java）不成功，未知原因，待解决 
--- src/main/java/com/cag/spring_boot_mybatis_demo/Application.java
+++ src/main/java/com/cag/spring_boot_mybatis_demo/Application.java
@@ -1,6 +1,5 @@
 package com.cag.spring_boot_mybatis_demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -16,23 +15,22 @@ import com.cag.spring_boot_mybatis_demo.domain.UserMapper;
 //写了 @RestController，就不用写 @Controller 和 下面每个路由的 @ResponseBody 了。 @RestController = @Controller + @ResponseBody。
 //@RestController
 @Controller
-//@SpringBootApplication 注解等价于以默认属性使用 @Configuration，@EnableAutoConfiguration和@ComponentScan
-//@SpringBootApplication
+// @SpringBootApplication注解等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan
+// @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-//@MapperScan
 public class Application {
-	
+
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
 	}
-	
+
 	@Autowired
 	UserMapper userMapper;
-	
+
 	@RequestMapping("/user")
 	@ResponseBody
 	String user() {
@@ -41,14 +39,15 @@ public class Application {
 		System.out.println(user.getName());
 		return user.getName();
 	}
-	
+
 	public static void main(String[] args) {
 		// 运行 Application.java 来启动Spring Boot项目，下面3种写法中选择一种
 		// 写法1：
-		 SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 		// 写法2：
-//		SpringApplication application = new SpringApplication(Application.class);
-//		application.run(args);
+		// SpringApplication application = new
+		// SpringApplication(Application.class);
+		// application.run(args);
 		// 写法3：
 		// new
 		// SpringApplicationBuilder().showBanner(false).sources(Application.class).run(args);
